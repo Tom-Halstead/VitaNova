a90696e5811a4ad06283cafa5ff2429b42d608db
Thomas Halstead <tom.michael.halstead@gmail.com>
2025-04-23 02:06:19 -0500

    First commit, backend created, diagrams created, user stories on GH

---
+++ vitanova-backend/src/main/java/com/vitanova/backend/VitanovaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vitanova.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitanovaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitanovaBackendApplication.class, args);
+	}
+
+}
---
+++ vitanova-backend/src/test/java/com/vitanova/backend/VitanovaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitanova.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitanovaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
