03fa3c04c072469d3921a2bfe2509447fe30fd5a
Thomas Halstead <tom.michael.halstead@gmail.com>
2025-04-24 02:52:44 -0500

    .

--- vitanova-backend/src/main/java/com/vitanova/backend/health/HealthController.java
+++ vitanova-backend/src/main/java/com/vitanova/backend/health/HealthController.java
@@ -1,13 +1,15 @@
 package com.vitanova.backend.health;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
-    public String health() {
-        return "OK";
+    public HttpStatus health() {
+        return HttpStatus.OK;
     }
 }
