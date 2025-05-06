8dfdbf3fab9ed736ec594b2faba088569e1409ca
Thomas Halstead <tom.michael.halstead@gmail.com>
2025-05-06 01:17:54 -0500

    React front-end improvements

--- vitanova-frontend/src/pages/DashboardItems/Timeline.jsx
+++ vitanova-frontend/src/pages/DashboardItems/Timeline.jsx
@@ -3,9 +3,10 @@ import InteractiveChart from "../../components/InteractiveChart";
 import { fetchSummary } from "../../api/DashboardApi";
 
 export default function Timeline() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetchSummary().then(setData);
-  }, []);
-  return <InteractiveChart data={data} />;
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetchSummary().then(setData);
+  // }, []);
+  // return<InteractiveChart data={data} />;
+  return <div style={{ color: "#6B7280" }}>Timeline coming soon.</div>;
 }
--- vitanova-frontend/src/pages/DashboardItems/Trends.jsx
+++ vitanova-frontend/src/pages/DashboardItems/Trends.jsx
@@ -3,9 +3,10 @@ import InteractiveChart from "../../components/InteractiveChart";
 import { fetchMoodTrend } from "../../api/DashboardApi";
 
 export default function Trends() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetchMoodTrend().then(setData);
-  }, []);
-  return <InteractiveChart data={data} />;
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetchMoodTrend().then(setData);
+  // }, []);
+  // return <InteractiveChart data={data} />;
+  return <div style={{ color: "#6B7280" }}>Trends coming soon.</div>;
 }
