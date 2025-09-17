d4650bc62783b560cb8a7f90976ce295b946188b
Thomas Halstead <tom.michael.halstead@gmail.com>
2025-09-17 01:10:33 -0500

    made a powershell script for dev

---
+++ vitanova-backend/src/main/resources/run-dev.ps1
@@ -0,0 +1,7 @@
+$env:SPRING_PROFILES_ACTIVE="dev"
+$env:DEV_DATASOURCE_URL="jdbc:postgresql://vitanovadb.c78c84w2om5e.us-east-2.rds.amazonaws.com:5432/vitanova"
+$env:DEV_DB_USERNAME="billythekid"
+$env:DEV_DB_PASSWORD="B1llyTheK1d1993!"
+$env:COGNITO_CLIENT_ID="2j12r8o421t03pnhhm0hjfi5qu"
+$env:COGNITO_CLIENT_SECRET="<your_dev_client_secret>"
+npm start
