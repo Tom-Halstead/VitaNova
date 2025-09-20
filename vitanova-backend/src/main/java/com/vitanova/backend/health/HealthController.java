fc8fb228f5e196c8ac24bf9d2cbc12fe3d98e4e2
Thomas Halstead <tom.michael.halstead@gmail.com>
2025-09-20 01:56:04 -0500

    Changes to health check

--- vitanova-backend/src/main/java/com/vitanova/backend/health/HealthController.java
+++ vitanova-backend/src/main/java/com/vitanova/backend/health/HealthController.java
@@ -2,6 +2,7 @@ package com.vitanova.backend.health;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping(value = "/health", produces = MediaType.TEXT_PLAIN_VALUE)
-    @ResponseStatus(HttpStatus.OK)
-    public String health() {
-        return "OK BIATCH!";
-    }
+    @GetMapping("/health")
+    public ResponseEntity<Void> health() { return ResponseEntity.ok().build(); }
 
 
 }
