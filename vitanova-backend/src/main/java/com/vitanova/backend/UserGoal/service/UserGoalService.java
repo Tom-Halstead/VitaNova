c169e0fc4703f30cdd1d3f81f9e791821964d1fe
Thomas Halstead <tom.michael.halstead@gmail.com>
2025-09-22 01:11:19 -0500

    Need to push and re-deploy

--- vitanova-backend/src/main/java/com/vitanova/backend/UserGoal/service/UserGoalService.java
+++ vitanova-backend/src/main/java/com/vitanova/backend/UserGoal/service/UserGoalService.java
@@ -127,7 +127,7 @@ public class UserGoalService {
                 int userId = userService.getUserIdByCognitoSub(cognitoSub);
                 UserGoalModel goal = userGoalRepo.findById(goalId)
                         .filter(g -> g.getUser().getUserId() == userId)
-                        .orElseThrow(() -> new IllegalArgumentException("No such goal"));
+                        .orElseThrow(() -> new IllegalArgumentException("No such goal!"));
                 userGoalRepo.delete(goal);
         }
 }
